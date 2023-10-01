2e54989b0360fb7a3fd32a555310298f14ca26a6
Mohamed Mourad <mohamed.m_2000@outlook.com>
2023-10-01 05:32:02 +0300

    update TwoSum.py

--- Two Sum/TwoSum.py
+++ Two Sum/TwoSum.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pos: int = 0  #current position
-        for i in range(len(nums)):
+        for j in range(len(nums)):
             #for loop to check sum of all pairs, current position with each other value
-            if pos == i:
-                continue
-            if nums[pos] + nums[i] == target:
-                #assuming that there's exactly one solution, we return once we find it
-                return [pos, i]
-            pos+=1
+            for i in range(len(nums)):
+                if j == i:
+                    continue
+                if nums[j] + nums[i] == target:
+                    #assuming that there's exactly one solution, we return once we find it
+                    return [j, i]
